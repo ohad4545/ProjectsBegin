e98ecc45b73f0ce0d38909c33c9a2b5860817654
ohad4545 <91246588+ohad4545@users.noreply.github.com>
2021-12-02 17:31:54 +0200

    Add files via upload

---
+++ deliveriesCompany_208113332/src/deliveriesCompany_208113332/DeliveryMemberMethods.java
@@ -0,0 +1,7 @@
+package deliveriesCompany_208113332;
+
+import java.util.ArrayList;
+
+public interface DeliveryMemberMethods {
+	Manager  whichManagerStrExists(String str,ArrayList<Manager> managers);
+}
