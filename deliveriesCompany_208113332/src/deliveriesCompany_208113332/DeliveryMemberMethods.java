c5cdea31267859ac129e880242c63aa48bcd2633
ohad4545 <91246588+ohad4545@users.noreply.github.com>
2021-12-03 22:46:00 +0200

    Delete deliveriesCompany_208113332 directory

--- deliveriesCompany_208113332/src/deliveriesCompany_208113332/DeliveryMemberMethods.java
+++
@@ -1,7 +0,0 @@
-package deliveriesCompany_208113332;
-
-import java.util.ArrayList;
-
-public interface DeliveryMemberMethods {
-	Manager  whichManagerStrExists(String str,ArrayList<Manager> managers);
-}
